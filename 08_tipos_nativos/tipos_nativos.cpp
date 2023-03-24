bcfc88bca860ece667f298c0c6018d2372fa765a
rzjprogramador <rzjprogramador@gmail.com>
2023-03-24 14:57:19 -0300

    refactor: organiza

--- 08_tipos_nativos/tipos_nativos.cpp
+++ 08_tipos_nativos/tipos_nativos.cpp
@@ -6,10 +6,3 @@ int main() {
 
   return 1;
 }
-
-/*
-string e comando de saida em console cout tem que serem atribuidos ao sistema std:: , 
-atribuicao ao sistema std é com 2vx 2 pontos ex: std:: o que vai entrar
-
-
-*/
