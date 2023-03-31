787d99b79a961ca6d54be449fce66df63fcb776f
rzjprogramador <rzjprogramador@gmail.com>
2023-03-31 15:21:21 -0300

    refactor: ok

--- Deno/Exemplos/Testers/main.ts
+++ Deno/Exemplos/Testers/main.ts
@@ -1,8 +1,6 @@
-// import { useSoma } from "./deps.ts";
 import { UseCalculos } from "./deps.ts";
 
 function main() {
-  // console.log(useSoma(100, 10));
   console.log(UseCalculos.soma(10, 5));
   console.log(UseCalculos.sub(10, 5));
   console.log(UseCalculos.mult(10, 5));
