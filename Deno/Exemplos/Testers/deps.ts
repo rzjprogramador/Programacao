6c44dc452b36688388f7f05099c7ecfd238ed53d
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-04-01 15:58:00 -0300

    tentando atualizar junto com o modulo

--- Deno/Exemplos/Testers/deps.ts
+++ Deno/Exemplos/Testers/deps.ts
@@ -1,4 +1,6 @@
-// deno-lint-ignore-file no-unused-vars
+// import { calculos } from "https://raw.githubusercontent.com/reinaldoreizao/modules/release_ts/calculos/math.ts";
+// import { calculos } from "https://github.com/reinaldoreizao/raw/modules/release_ts/release_ts/calculos/math.ts";
 
-import { Calculos } from "https://raw.githubusercontent.com/rzjprogramador/modules/v2.0.0/ts_exemplos/calculos/index.ts";
-export const UseCalculos = Calculos;
+import { calculos } from "https://raw.githubusercontent.com/reinaldoreizao/modules/release_ts/release_ts/calculos/math.ts";
+
+export const useCalculos = calculos;
--- Deno/Exemplos/Testers/main.ts
+++ Deno/Exemplos/Testers/main.ts
@@ -1,9 +1,7 @@
-import { UseCalculos } from "./deps.ts";
+import { useCalculos } from "./deps.ts";
 
 function main() {
-  console.log(UseCalculos.soma(10, 5));
-  console.log(UseCalculos.sub(10, 5));
-  console.log(UseCalculos.mult(10, 5));
+  console.log(useCalculos.soma(10, 5));
 }
 
 main();
