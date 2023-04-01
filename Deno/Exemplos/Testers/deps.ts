d6856fc7360142cfaee8f77be7f33f3c2a3b2d48
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-04-01 20:22:22 -0300

    refactor: ok

--- Deno/Exemplos/Testers/deps.ts
+++
@@ -1,6 +0,0 @@
-// import { calculos } from "https://raw.githubusercontent.com/reinaldoreizao/modules/release_ts/calculos/math.ts";
-// import { calculos } from "https://github.com/reinaldoreizao/raw/modules/release_ts/release_ts/calculos/math.ts";
-
-import { calculos } from "https://raw.githubusercontent.com/reinaldoreizao/modules/release_ts/release_ts/calculos/math.ts";
-
-export const useCalculos = calculos;
--- Deno/Exemplos/Testers/main.ts
+++
@@ -1,7 +0,0 @@
-import { useCalculos } from "./deps.ts";
-
-function main() {
-  console.log(useCalculos.soma(10, 5));
-}
-
-main();
