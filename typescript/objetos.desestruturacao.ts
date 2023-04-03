333d0ab28ad8483b66172b80afb43108b7ba9149
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-04-03 14:19:00 -0300

    feat: objetos

--- typescript/objetos.desestruturacao.ts
+++ typescript/objetos.desestruturacao.ts
@@ -10,29 +10,6 @@ const user = {
   },
 };
 
-// Manipulacao Objeto
-/* *************************************************** */
-
-// operador_boleano 'in'  :: verifica se existe a informacao chave key dentro do objeto
-// console.log("nome" in user);
-// console.log("Reinaldo" in user.nome); // TODO: key.value por enquanto da erro.
-
-/* *************************************************** */
-
-// Object.keys(obj) // retorna um vetor com todas chaves do objeto passado
-// console.log(Object.keys(user));
-
-// Object.values(obj) // retorna um vetor com todos os valores do objeto passado - obs: os sub_objetos retornam um objeto com chave e valor.
-// console.log(Object.values(user));
-
-// Object.entries(obj) // retorna uma matriz, separando cada chave e valor em vetores/array de string
-// console.log(Object.entries(user));
-
-// Mostrar em JSON estruturas de Objeto com o metodo do Obj JSON :: JSON.stringify(alvo) ex: quando retornado objetos [ Object, Object ]
-// console.log(JSON.stringify(Object.values(user)));
-
-/* *************************************************** */
-
 // DESESTRUTURACAO
 
 /*
