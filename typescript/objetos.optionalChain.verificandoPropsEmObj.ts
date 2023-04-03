cf0eff780fb93943540857941e5230aa304dcb31
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-04-03 14:45:57 -0300

    feat: verificacao de campos de objeto para uso

--- typescript/objetos.optionalChain.verificandoPropsEmObj.ts
+++ typescript/objetos.optionalChain.verificandoPropsEmObj.ts
@@ -1,5 +1,6 @@
 // deno-lint-ignore-file no-unused-vars no-explicit-any
-// OBJETOS
+
+// OBJETOS - optionalChain :: verificacao de campos de objeto para uso
 
 const user = {
   nome: "Reinaldo",
@@ -10,17 +11,34 @@ const user = {
   // },
 };
 
+const obj2 = {
+  nome: "Reinaldo",
+  idade: 45,
+  endereco: {
+    rua: null,
+  },
+};
+
 // forma manual legado: >> se no obj a prop existe faça isto, senao faça estaOutraOpcao
 // console.log(user.endereco ? endereco : "nao existe");
 
 /*
 optionalChain
-conceito: verificando se existe props no objeto para uso ... se nao existe a prop ele retorna undefined mas nao estoura erro e crachea trava o codigo.
+conceito: verificacao de campos de objeto para uso.
+
+beneficio: vai verificar se existem as props no objeto usado e se nao existir nao vai travar/crachear a saida do codigo, vai somente retornar undefined se nao existir ou nulo senao tiver valorValido e podemos pegar estes retornos invalidos e fazer algum tratamento.
 
 dica: faça a verificacao optionalChain a cada profundidade do objeto.
 
 sintaxe: objeto <?> <.>
-narracao: seExistir ? abra<.> proximaProp
+
+narracao: seExistir ? abra . proximaProp
+
 macete: é sempre a interrogacao antes do ponto ex: obj?.prop1
+
 */
+
+// verificando se as props existem no uso
 console.log(user?.endereco);
+console.log(obj2?.endereco?.rua);
+console.log(obj2?.endereco?.naoExisteSubKey);
